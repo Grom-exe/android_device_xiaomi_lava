7784290f16a4280c5819de964a5018b781ab6401
Grom-exe <103995190+Grom-exe@users.noreply.github.com>
2022-06-27 10:58:55 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/voltage_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    voltage_lava-user \
+    voltage_lava-userdebug \
+    voltage_lava-eng
